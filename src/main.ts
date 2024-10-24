3f749cecc1ec8cb6c5a1aa92ed6950a9f2524955
Dave <17897083+carrickkv2@users.noreply.github.com>
2024-10-24 15:22:28 +0000

    Add listener

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,27 @@ import {
   getStartingURLs,
 } from "./helpers/functions/serviceWorker";
 
+chrome.runtime.onMessageExternal.addListener(function (request, _sender, _sendResponse) {
+  if (request.data)
+    async () => {
+      try {
+        const response = await fetch("/bb.json");
+        const rawInstructions = await response.text();
+        const startingUrls = getStartingURLs(rawInstructions);
+
+        for (const url of startingUrls) {
+          await chrome.storage.session.set({ [`startingUrl_${url}`]: url });
+          await chrome.storage.session.set({ [`instructions_${url}`]: rawInstructions });
+        }
+
+        await addListenersForStartingUrls();
+        await CreateNewWindowOrTab(rawInstructions);
+      } catch (error) {
+        console.error("Error starting instruction processor:", error);
+      }
+    };
+});
+
 document.getElementById("startButton")!.addEventListener("click", async () => {
   try {
     const response = await fetch("/bb.json");
